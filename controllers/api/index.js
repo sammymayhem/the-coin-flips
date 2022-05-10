853fd303bb3a90f59470fdf5d0a951a2e57c591d
Matthew Carlson <carlson.matt.r@gmail.com>
2022-05-10 11:21:10 -0700

    morning work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const usersRouter = require("./users-router");
+const predictionRouter = require("./prediction-router");
 const exampleRouter = require("./example-router");
 
 router.use("/users", usersRouter);
+router.use("/prediction", predictionRouter);
 router.use("/example", exampleRouter);
 
 module.exports = router;
