f6c917df8b38decf3f3acb3be26de0b609d52da5
JK <jrileykruger@gmail.com>
2022-05-12 15:45:52 -0700

    added prediction functionality, server call and db entry

--- controllers/api/prediction-router.js
+++ controllers/api/prediction-router.js
@@ -13,15 +13,21 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
+    console.log(req.body);
+    console.log(req.session.userId);
+
     const predictionData = await Prediction.create({
       ticker: req.body.ticker,
       start_price: req.body.start_price,
       start_time: req.body.start_time,
-      predicted_price: req.body.predicted_time,
+      predicted_price: req.body.predicted_price,
       predicted_time: req.body.predicted_time,
+      user_id: req.session.userId,
     });
+
     res.status(200).json(predictionData);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
--- public/js/predict.js
+++ public/js/predict.js
@@ -1,58 +1,118 @@
-// just going to be placeholders for now
-const handlePredictionPost = async (event) => {
-  console.log(event);
-  event.preventDefault();
-  try {
-    //all the document selectors are placeholders atm
-    const prediction = {
-      selectedTicker: document.getElementById("ticker"),
-      // selectedPrice: document.querySelector("ticker-price"),
-      // selectedTime: document.querySelector("current-time"),
-      // predictedPrice: document.querySelector("prediction-price"),
-      // predictedTime: document.querySelector("prediction-time")
-    };
-    console.log(prediction.selectedTicker.textContent);
-    // if (!prediction.predictedPrice || !prediction.predictedTime){
-    //     alert("You must predict both a time AND a price to proceed");
-    //     return;
-    // };
+const ctx = document.getElementById("chart").getContext("2d");
+const currentPrice = document.getElementById("current-price");
+const ticker = document.getElementById("ticker-symbol").textContent;
+const predictionPreview = document.getElementById("prediction-preview");
 
-    // const predictResponse = await fetch ("/api/prediction", {
-    //     method: "POST",
-    //     body: prediction,
-    //     headers: {
-    //         "Content-Type": "application/json; charset=UTF-8"
-    //     }
-    // });
-    // if (!predictResponse.ok) {
-    //     alert ("Prediction failed to log.  Please try again");
-    //     return;
-    // }
+// let clickX;
+// let clickY;
 
-    // window.location.replace("/predictions")
-  } catch (err) {
-    console.log(err);
-  }
+const validateClick = (x) => {
+  return x > moment().format("x");
 };
-const searchTicker = async () => {
+
+const getChart = async () => {
   try {
-    const tickerText = document.getElementById("ticker-text").value;
-    console.log(tickerText);
-    const response = await fetch("/api/prediction/ticker", {
+    const response = await fetch("/chart", {
       method: "POST",
-      body: JSON.stringify({ tickerText }),
+      body: JSON.stringify({ ticker }),
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
+      },
+    });
+    const axisdata = await response.json();
+
+    currentPrice.textContent = axisdata.y[axisdata.y.length - 1];
+
+    const chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: axisdata.x,
+        datasets: [
+          {
+            borderColor: "#00fff9",
+            data: axisdata.y,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: "time",
+            time: {
+              displayFormats: {
+                day: "MMM D",
+              },
+            },
+            ticks: {
+              color: "#D3D3D3",
+            },
+          },
+          y: {
+            ticks: {
+              color: "#D3D3D3",
+            },
+          },
+        },
+        elements: {
+          point: {
+            radius: 0,
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        onClick: (e) => {
+          const canvasPosition = Chart.helpers.getRelativePosition(e, chart);
+          const clickX = Math.round(
+            chart.scales.x.getValueForPixel(canvasPosition.x)
+          );
+          const clickY =
+            Math.round(
+              chart.scales.y.getValueForPixel(canvasPosition.y) * 100
+            ) / 100;
+
+          const validClick = validateClick(clickX);
+          const predDate = moment(clickX, "x").format("MMM D");
+
+          if (validClick) {
+            predictionPreview.innerHTML = `
+            <h5 id="prediction-header">Prediction</h5>
+            <h6>${ticker}:  $${clickY} @ ${predDate}</h6>
+            <button type="button" class="btn btn-primary" id="predict-btn">Predict</button>`;
+            document
+              .getElementById("predict-btn")
+              .addEventListener("click", async () => {
+                try {
+                  const predictionData = {
+                    ticker,
+                    start_price: currentPrice.textContent,
+                    start_time: moment().format("x"),
+                    predicted_price: clickY,
+                    predicted_time: clickX,
+                  };
+                  const response = await fetch("/api/prediction", {
+                    method: "POST",
+                    body: JSON.stringify(predictionData),
+                    headers: {
+                      "Content-Type": "application/json; charset=UTF-8",
+                    },
+                  });
+                } catch (err) {
+                  console.log(err);
+                }
+                location.replace("/predictions");
+              });
+          }
+        },
       },
     });
   } catch (err) {
     console.log(err);
   }
 };
-// will change query selector to be what the prediction selector will be
-document
-  .getElementById("submitButton")
-  .addEventListener("click", handlePredictionPost);
-document
-  .getElementById("ticker-search")
-  .addEventListener("click", searchTicker);
+
+const sendPrediction = async () => {};
+
+getChart();
